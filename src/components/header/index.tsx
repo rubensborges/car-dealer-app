8e06eb3aea704a310107b3df0957cfa75092cdd7
Rubens Borges <contact@rubensborges.dev>
2024-11-12 12:58:05 -0300

    feat: added hiding behavior for header component on mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <h1>header</h1>;
+  return (
+    <header className="fixed max-sm:hidden top-0 p-6 bg-zinc-700 w-full ">
+      <main className="text-center text-xl">Cars Today</main>
+    </header>
+  );
 }
