775ef56f5818cf97f2fa96d24683509d4d31394a
Rubens Borges <contact@rubensborges.dev>
2024-11-12 12:58:32 -0300

    feat: added a margin top for the children element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <div className="sm:mt-24">{children}</div>
         <Footer />
       </body>
     </html>
