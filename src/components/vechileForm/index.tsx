3a3de84b351568af67532bb8c9a340689dab7957
Rubens Borges <contact@rubensborges.dev>
2024-11-12 12:38:16 -0300

    feat: vehicle form component created

---
+++ src/components/vechileForm/index.tsx
@@ -0,0 +1,8 @@
+export default function VehicleForm() {
+  return (
+    <form action="">
+      <select name="make" id="make"></select>
+      <select name="year" id="year"></select>
+    </form>
+  );
+}
