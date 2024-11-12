bb382445c0ac54269688e1dca0b0b51a6cf9de4f
Rubens Borges <contact@rubensborges.dev>
2024-11-12 20:35:00 -0300

    feat: added rule for disabling based on props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -9,7 +9,11 @@ export default function Button({ selectedMakeId, selectedYear }: Props) {
     <Link href={`result/${selectedMakeId}/${selectedYear}`} passHref>
       <button
         disabled={!selectedMakeId || !selectedYear}
-        className="p-2 w-full bg-[#9a9a9a] rounded-lg hover:bg-[#9a9a9a9a] text-white text-xl font-semibold text-center"
+        className={` ${
+          selectedMakeId && selectedYear
+            ? 'bg-[#9a9a9a] text-white'
+            : 'bg-[#9a9a9a9a] text-white/30'
+        } p-2 w-full  rounded-lg hover:bg-[#9a9a9a9a]  text-xl font-semibold text-center`}
       >
         Next
       </button>
