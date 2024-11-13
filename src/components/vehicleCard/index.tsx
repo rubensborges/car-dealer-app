c78fe93be6f857ec39d27543e2240eafb00852ca
Rubens Borges <contact@rubensborges.dev>
2024-11-13 16:04:07 -0300

    feat: fetching the data on vehicle card component

--- src/components/vehicleCard/index.tsx
+++ src/components/vehicleCard/index.tsx
@@ -1,10 +1,24 @@
+import { fetchVehicleDetails } from '@/services/vehicleServices/vehicleDetails';
+import { useEffect, useState } from 'react';
+
 type Props = {
-  modelName?: string;
-  modelYear?: number;
+  modelName: string;
+  modelYear: number | string | undefined;
   price?: string;
-  image?: string | undefined;
 };
 export default function VehicleCard({ modelName, modelYear, price }: Props) {
+  const [carDetails, setCarDetails] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetchVehicleDetails(modelName, modelYear);
+        console.log(req);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <main className="">
       <section></section>
