fc91c34185d3223a5a481ed61d717e3c1484f345
Rubens Borges <contact@rubensborges.dev>
2024-11-13 11:51:10 -0300

    feat: getting the data from the params to the fetch service

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,3 +1,31 @@
-export default function ResultPage() {
-  return <h1>result car</h1>;
+'use client';
+type Params = {
+  year: string | number;
+  makeId: string | number;
+};
+
+import { ResponseGetVehicleModel } from '@/interfaces/vehicleInterface';
+import { fetchVehicleModelsByIdAndYear } from '@/services/vehicleServices/modelsService';
+import React, { useEffect } from 'react';
+
+export default function ResultPage({ params }: { params: Promise<Params> }) {
+  const resolvedParams = React.use(params);
+
+  const year = resolvedParams.year;
+  const id = resolvedParams.makeId;
+  useEffect(() => {
+    const fetchData = async () => {
+      const req: ResponseGetVehicleModel = await fetchVehicleModelsByIdAndYear(id, year);
+      console.log(req.Results);
+    };
+    fetchData();
+  }, []);
+
+  console.log(year, id);
+
+  return (
+    <div>
+      Year: {year}, ID: {id}
+    </div>
+  );
 }
