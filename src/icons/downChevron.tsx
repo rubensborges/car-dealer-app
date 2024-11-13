8e252024aff70c8aecfc754cc4d93eac259b70b6
Rubens Borges <contact@rubensborges.dev>
2024-11-12 21:08:22 -0300

    chore: added components to src folder

---
+++ src/icons/downChevron.tsx
@@ -0,0 +1,19 @@
+export default function IconDownChevron() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-full"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        id="SVGRepo_iconCarrier"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m6 9 6 6 6-6"
+      ></path>
+    </svg>
+  );
+}
