11372c1c926a84d908b42ce33def3557d31c2f97
Rubens Borges <contact@rubensborges.dev>
2024-11-13 14:01:47 -0300

    feat: loader component created

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+  return (
+    <main>
+      <section className="bg-gray-300 w-full h-40 rounded-lg animate-pulse"></section>
+      <h1 className="w-full bg-gray-300 animate-pulse p-2 rounded-lg"></h1>
+      <h1 className="w-full bg-gray-300 animate-pulse p-2 rounded-lg"></h1>
+      <h1 className="w-full bg-gray-300 animate-pulse p-2 rounded-lg"></h1>
+      <h1 className="w-full bg-gray-300 animate-pulse p-2 rounded-lg"></h1>
+    </main>
+  );
+}
