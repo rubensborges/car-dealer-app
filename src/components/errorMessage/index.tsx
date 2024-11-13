f6d710543678432ea8fed584e916cc68fb9ad7a7
Rubens Borges <contact@rubensborges.dev>
2024-11-13 15:26:35 -0300

    feat: created error message component

---
+++ src/components/errorMessage/index.tsx
@@ -0,0 +1,10 @@
+export default function ErrorMessage() {
+  return (
+    <main>
+      <div className="text-3xl flex flex-col gap-4 mt-10 font-semibold text-center">
+        <p>We were not able to find any car models.</p>
+        <p>Please, try again.</p>
+      </div>
+    </main>
+  );
+}
