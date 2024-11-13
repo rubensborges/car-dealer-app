97207614c8f514773071068be9fc45c3f1fa9349
Rubens Borges <contact@rubensborges.dev>
2024-11-13 16:29:18 -0300

    feat: added new icons

---
+++ src/icons/engine.tsx
@@ -0,0 +1,16 @@
+export default function IconEngine() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-full"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M3 10v6M12 5v3M10 5h4M5 13H3M6 10h2l2-2h3.382a1 1 0 0 1 .894.553l1.448 2.894a1 1 0 0 0 .894.553H18v-2h2a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-2v-2h-3v2a1 1 0 0 1-1 1h-3.465a1 1 0 0 1-.832-.445L8 16H6z"></path>
+    </svg>
+  );
+}
