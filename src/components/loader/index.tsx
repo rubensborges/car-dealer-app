d53cee8f081a5b2952b6e3bb9fe99915ac0f10b1
Rubens Borges <contact@rubensborges.dev>
2024-11-13 17:38:52 -0300

    fix: altered loader layout

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="flex space-x-2 w-full h-full justify-center items-center">
+    <div className="flex space-x-2 mt-60 w-full h-full justify-center items-center">
       <span className="sr-only">Loading...</span>
       <div className="size-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
       <div className="size-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
