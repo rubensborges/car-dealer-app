0e08ca8da02c2a21f16c22e28f31d95adec88093
Rubens Borges <contact@rubensborges.dev>
2024-11-13 14:01:33 -0300

    fix: changing on the overall header layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
-    <header className="fixed max-sm:hidden top-0 p-6 bg-zinc-700 w-full ">
-      <main className="text-center text-xl">Cars Today</main>
+    <header className=" max-sm:hidden flex items-center gap-10 py-6 px-24 text-zinc-700 bg-white/80 w-full ">
+      <main className=" text-3xl">Cars Today</main>
+      <Link href={'/'}>
+        <section className="hover:text-black">Home</section>
+      </Link>
     </header>
   );
 }
