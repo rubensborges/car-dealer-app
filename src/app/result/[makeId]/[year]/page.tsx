4e53baf3ad4914987cebe3c8dfd23f1101c8c4b2
Rubens Borges <contact@rubensborges.dev>
2024-11-13 15:27:01 -0300

    feat: added error handling and suspense react component validation

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -4,33 +4,57 @@ type Params = {
   makeId: string | number;
 };
 
+import ErrorMessage from '@/components/errorMessage';
 import Loader from '@/components/loader';
+import VehicleCard from '@/components/vehicleCard';
+import IconLeftCheveon from '@/icons/leftChevron';
 import { ResponseGetVehicleModel, VehicleModels } from '@/interfaces/vehicleInterface';
 import { fetchVehicleModelsByIdAndYear } from '@/services/vehicleServices/modelsService';
-import React, { Suspense, useEffect, useState } from 'react';
+import Link from 'next/link';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 
 export default function ResultPage({ params }: { params: Promise<Params> }) {
   const [vehicleModel, setVehicleModel] = useState<VehicleModels[]>([]);
+  const [error, setError] = useState(false);
 
   const resolvedParams = React.use(params);
 
+  const ErrorMessage = lazy(() => import('@/components/errorMessage'));
+
   const year = resolvedParams.year;
   const id = resolvedParams.makeId;
+
   useEffect(() => {
     const fetchData = async () => {
-      const req: ResponseGetVehicleModel = await fetchVehicleModelsByIdAndYear(id, year);
-      setVehicleModel(req.Results);
+      try {
+        const req: ResponseGetVehicleModel = await fetchVehicleModelsByIdAndYear(id, year);
+        if (req.Results.length === 0) {
+          setError(true);
+          return;
+        }
+        setVehicleModel(req.Results);
+      } catch (error) {
+        setError(true);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <div>
-      {vehicleModel.map((vehicle, index) => (
-        <Suspense key={index} fallback={<Loader />}>
-          <div key={index}>{vehicle.Model_Name}</div>
+    <div className="bg-zinc-200 vh-[100] max-w-[600px] mx-auto rounded-b-[40px] px-4 pt-4 pb-20 ">
+      <Link href={'/'}>
+        <div className="size-10 hover:bg-slate-600/80 rounded-lg p-2 bg-slate-600/30">
+          <IconLeftCheveon />
+        </div>
+      </Link>
+      <div className="mt-10 mx-auto flex items-center flex-col h-full">
+        <Suspense fallback={<Loader />}>
+          {vehicleModel.map(async (vehicle, index) => (
+            <VehicleCard key={index} modelName={vehicle.Model_Name} />
+          ))}
         </Suspense>
-      ))}
+      </div>
+      <Suspense fallback={<Loader />}>{error && <ErrorMessage />}</Suspense>
     </div>
   );
 }
