172c5893183084028c96d833c2b285c30f40b32a
Rubens Borges <contact@rubensborges.dev>
2024-11-13 17:39:19 -0300

    feat: correctly handling the data coming from two different apis

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,63 +1,83 @@
 'use client';
+import Loader from '@/components/loader';
+import VehicleCard from '@/components/vehicleCard';
+import IconLeftCheveon from '@/icons/leftChevron';
+import { ResponseGetVehicleModel, VehicleDetails } from '@/interfaces/vehicleInterface';
+import { fetchVehicleModelsByIdAndYear } from '@/services/vehicleServices/modelsService';
+import { fetchVehicleDetails } from '@/services/vehicleServices/vehicleDetails';
+import Link from 'next/link';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
+
 type Params = {
   year: string | number;
   makeId: string | number;
 };
 
-import ErrorMessage from '@/components/errorMessage';
-import Loader from '@/components/loader';
-import VehicleCard from '@/components/vehicleCard';
-import IconLeftCheveon from '@/icons/leftChevron';
-import { ResponseGetVehicleModel, VehicleModels } from '@/interfaces/vehicleInterface';
-import { fetchVehicleModelsByIdAndYear } from '@/services/vehicleServices/modelsService';
-import Link from 'next/link';
-import React, { lazy, Suspense, useEffect, useState } from 'react';
-
 export default function ResultPage({ params }: { params: Promise<Params> }) {
-  const [vehicleModel, setVehicleModel] = useState<VehicleModels[]>([]);
+  const [vehicleDetails, setVehicleDetails] = useState<(VehicleDetails | undefined)[]>([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const resolvedParams = React.use(params);
-
-  const ErrorMessage = lazy(() => import('@/components/errorMessage'));
-
   const year = resolvedParams.year;
   const id = resolvedParams.makeId;
 
+  const ErrorMessage = lazy(() => import('@/components/errorMessage'));
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchAllVehicleDetails = async () => {
       try {
         const req: ResponseGetVehicleModel = await fetchVehicleModelsByIdAndYear(id, year);
         if (req.Results.length === 0) {
           setError(true);
           return;
         }
-        setVehicleModel(req.Results);
-      } catch (error) {
+
+        const detailsPromises = req.Results.map(async (vehicle) => {
+          const modelName = vehicle.Model_Name;
+          const details = await fetchVehicleDetails(modelName, year);
+          return details ? { ...details, model: modelName } : undefined;
+        });
+
+        const details = await Promise.all(detailsPromises);
+        setVehicleDetails(details);
+        setLoading(false);
+      } catch (err) {
         setError(true);
+        setLoading(false);
       }
     };
-    fetchData();
+
+    fetchAllVehicleDetails();
   }, []);
 
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div className=" vh-[100] max-w-[600px] mx-auto rounded-b-[40px] px-4 pt-4 pb-20 ">
-      <Link href={'/'}>
-        <div className="size-10 hover:bg-slate-600/80 rounded-lg p-2 bg-slate-600/30">
-          <IconLeftCheveon />
-        </div>
-      </Link>
-      <article className="md:text-xl text-md font-semibold text-center mt-8">
-        Showing all {vehicleModel[0] && vehicleModel[0].Make_Name} models from the year {year}
-      </article>
-      <div className="mt-5 mx-auto gap-4 flex items-center flex-col h-full">
-        <Suspense fallback={<Loader />}>
-          {vehicleModel.map(async (vehicle, index) => (
-            <VehicleCard key={index} modelYear={year} modelName={vehicle.Model_Name} />
+    <Suspense fallback={<Loader />}>
+      <div className="vh-[100] max-w-[600px] mx-auto rounded-b-[40px] px-4 pt-4 pb-20">
+        <Link href={'/'}>
+          <div className="size-10 hover:bg-slate-600/80 rounded-lg p-2 bg-slate-600/30">
+            <IconLeftCheveon />
+          </div>
+        </Link>
+        <article className="md:text-xl text-md font-semibold text-center mt-8">
+          Showing all {vehicleDetails[0]?.make || ''} models from the year {year}
+        </article>
+        <div className="mt-5 mx-auto max-h-[70vh] overflow-auto gap-4 flex items-center flex-col h-full">
+          {vehicleDetails.map((details, index) => (
+            <VehicleCard
+              key={index}
+              modelYear={year}
+              modelName={details?.model}
+              carDetails={details}
+            />
           ))}
-        </Suspense>
+        </div>
+        {error && <ErrorMessage />}
       </div>
-      <Suspense fallback={<Loader />}>{error && <ErrorMessage />}</Suspense>
-    </div>
+    </Suspense>
   );
 }
