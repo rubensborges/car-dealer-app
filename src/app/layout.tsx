aced662a8022755df2a761b7473ba9df4645cff7
Rubens Borges <contact@rubensborges.dev>
2024-11-13 15:27:22 -0300

    chore: removed footer component from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import Footer from '@/components/footer';
 import Header from '@/components/header';
 
 const geistSans = localFont({
@@ -30,7 +29,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         <div>{children}</div>
-        <Footer />
       </body>
     </html>
   );
