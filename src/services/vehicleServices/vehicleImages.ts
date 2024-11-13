30301e540da2110a19ebc4a95507125e72b232a7
Rubens Borges <contact@rubensborges.dev>
2024-11-13 16:02:59 -0300

    chore: deleted service

--- src/services/vehicleServices/vehicleImages.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export async function fetchVehicleImages(name: string) {
-  const req = await axios.get(`https://www.googleapis.com/customsearch/v1?${name}`);
-  return req.data;
-}
