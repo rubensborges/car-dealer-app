9aea5d847b0d7f90ca170265ce85f7bccc679c38
Rubens Borges <contact@rubensborges.dev>
2024-11-13 17:42:18 -0300

    feat: implemented new validations

--- src/utils/formatText.ts
+++ src/utils/formatText.ts
@@ -4,6 +4,8 @@ export default function formatText(text: string | undefined) {
       return 'Rear Wheel Drive';
     case '4wd':
       return 'All Wheel Drive';
+    case 'awd':
+      return 'All Wheel Drive';
     case 'fwd':
       return 'Front Wheel Drive';
     case 'a':
