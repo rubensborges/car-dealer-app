5df3128be03bcf2d2e31bb299afc1720ddf1b38d
Rubens Borges <contact@rubensborges.dev>
2024-11-13 17:37:49 -0300

    fix: altered logic for formating text

---
+++ src/utils/formatText.ts
@@ -0,0 +1,16 @@
+export default function formatText(text: string | undefined) {
+  switch (text) {
+    case 'rwd':
+      return 'Rear Wheel Drive';
+    case '4wd':
+      return 'All Wheel Drive';
+    case 'fwd':
+      return 'Front Wheel Drive';
+    case 'a':
+      return 'Automatic';
+    case 'm':
+      return 'Manual';
+    default:
+      return undefined;
+  }
+}
