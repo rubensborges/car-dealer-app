cf1141dd5b29d0d596d38108199bff7375794e12
Rubens Borges <contact@rubensborges.dev>
2024-11-13 11:50:44 -0300

    feat: created a new interface for car models fetching

--- src/interfaces/vehicleInterface.ts
+++ src/interfaces/vehicleInterface.ts
@@ -2,3 +2,14 @@ export interface IVehicleForm {
   MakeId: number;
   MakeName: string;
 }
+
+interface VehicleModels {
+  Make_ID: number;
+  Make_Name: string;
+  Model_ID: number;
+  Model_Name: string;
+}
+
+export interface ResponseGetVehicleModel {
+  Results: VehicleModels[];
+}
